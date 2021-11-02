e6fe43147eb901ac5fde16add3572c840cf5dff3
justinfisherrr <74376317+justinfisherrr@users.noreply.github.com>
2021-11-02 16:13:57 -0700

    Queue functionality. Clear Search.

--- front-end/src/components/apple-player/ApplePlayer.jsx
+++ front-end/src/components/apple-player/ApplePlayer.jsx
@@ -1,128 +1,165 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { SocketContext } from '../../context/socketContext';
-import { AppleMusicContext } from '../../context/AppleMusicContext';
-import './apple-player.scss';
-import AppleDisplay from '../apple-display/AppleDisplay';
-import AppleQueue from '../apple-queue/AppleQueue';
-import AppleSearch from '../apple-search/AppleSearch';
+import React, { useEffect, useContext, useState } from "react";
+import { SocketContext } from "../../context/socketContext";
+import { AppleMusicContext } from "../../context/AppleMusicContext";
+import "./apple-player.scss";
+import AppleDisplay from "../apple-display/AppleDisplay";
+import AppleQueue from "../apple-queue/AppleQueue";
+import AppleSearch from "../apple-search/AppleSearch";
 function ApplePlayer({ lobby_id }) {
-	const socket = useContext(SocketContext);
-	const [musicKit] = useContext(AppleMusicContext);
-	const [queue, setQueue] = useState([]);
+  const socket = useContext(SocketContext);
+  const [musicKit, setMusicKit] = useContext(AppleMusicContext);
+  const [queue, setQueue] = useState([]);
 
-	useEffect(() => {
-		// Apple Socket Functions
-		socket.on('playApple', async () => {
-			await play();
-		});
+  useEffect(() => {
+    // Apple Socket Functions
+    socket.on("playApple", async () => {
+      await play();
+    });
 
-		// Apple Player Controllers
-		let play = async () => {
-			await musicKit.authorize().then(async () => {
-				await musicKit.play();
-				musicKit.player.volume = 0.05;
-			});
-		};
-	}, [socket, musicKit]);
+    // Apple Player Controllers
+    let play = async () => {
+      await musicKit.authorize().then(async () => {
+        await musicKit.play();
+        musicKit.player.volume = 0.05;
+      });
+    };
+  }, []);
 
-	function playSong() {
-		socket.emit('playSong', lobby_id);
-	}
-	async function setSong(song) {
-		await musicKit.authorize().then(async () => {
-			let musicQueue = musicKit.player.queue;
+  function playSong() {
+    socket.emit("playSong", lobby_id);
+  }
+  async function removeItemFromQueue(index) {
+    //Changing the display of the queue
+    let tempQueue = queue;
+    tempQueue.splice(index, 1);
+    await setQueue([...tempQueue]);
 
-			musicQueue.append(song);
+    //Changing MusicKit Queue
+    let currentQueue = musicKit.player.queue.items;
+    currentQueue.splice(index, 1);
+    await musicKit.authorize().then(async () => {
+      await musicKit.setQueue({
+        songs: currentQueue,
+      });
+    });
+  }
+  async function removeAllItemsFromQueue() {
+    //Changing the display of the queue
 
-			setQueue([
-				...queue,
-				{
-					attributes: {
-						artistName: song.attributes.artistName,
-						name: song.attributes.name,
-					},
-				},
-			]);
-		});
-	}
-	async function setAlbum(album) {
-		let currentQueue = musicKit.player.queue.items;
-		let arrayQueue = [];
-		currentQueue.forEach(({ id }) => {
-			arrayQueue.push(id);
-		});
+    await setQueue([]);
+    //Changing MusicKit Queue
+    await musicKit.authorize().then(async () => {
+      await musicKit.setQueue({
+        songs: [],
+      });
+    });
+  }
 
-		//Added by album id (issue: clears entire queue before adding)
-		await musicKit.authorize().then(async () => {
-			await musicKit.setQueue({
-				album: album.id,
-			});
-		});
+  async function playSongByIndex(index) {
+    await musicKit.authorize().then(async () => {
+      await musicKit.changeToMediaAtIndex(index);
+      musicKit.player.volume = 0.05;
+    });
+    playSong();
+  }
+  console.log(musicKit);
+  async function setSong(song) {
+    await musicKit.authorize().then(async () => {
+      let musicQueue = musicKit.player.queue;
 
-		//If we have existing songs, we will push those
-		let newQueue = musicKit.player.queue.items;
-		if (arrayQueue.length > 0) {
-			newQueue.forEach(({ id }) => {
-				arrayQueue.push(id);
-			});
+      musicQueue.append(song);
 
-			await musicKit.authorize().then(async () => {
-				await musicKit.setQueue({
-					songs: arrayQueue,
-				});
-			});
-		}
-		//Displaying everysong
-		let displayQueue = [];
-		newQueue.forEach(({ attributes }) => {
-			displayQueue.push({
-				attributes: {
-					artistName: attributes.artistName,
-					name: attributes.name,
-				},
-			});
-		});
-		setQueue([...queue, ...displayQueue]);
-	}
+      setQueue([
+        ...queue,
+        {
+          attributes: {
+            artistName: song.attributes.artistName,
+            name: song.attributes.name,
+          },
+        },
+      ]);
+    });
+  }
+  async function setAlbum(album) {
+    let currentQueue = musicKit.player.queue.items;
+    let arrayQueue = [];
+    currentQueue.map(({ id }) => {
+      arrayQueue.push(id);
+    });
 
-	// TEMP PLAYER CONTROLS --- FOR TESTING
-	let nextSong = async () => {
-		await musicKit.authorize().then(async () => {
-			await musicKit.skipToNextItem();
-		});
-	};
-	let prevSong = async () => {
-		await musicKit.authorize().then(async () => {
-			await musicKit.skipToPreviousItem();
-		});
-	};
-	let pauseSong = async () => {
-		await musicKit.authorize().then(async () => {
-			await musicKit.pause();
-		});
-	};
+    //Added by album id (issue: clears entire queue before adding)
+    await musicKit.authorize().then(async () => {
+      await musicKit.setQueue({
+        album: album.id,
+      });
+    });
 
-	// TEMP PLAYER CONTROLS --- FOR TESTING
+    //If we have existing songs, we will push those
+    let newQueue = musicKit.player.queue.items;
+    if (arrayQueue.length > 0) {
+      newQueue.map(({ id }) => {
+        arrayQueue.push(id);
+      });
 
-	return (
-		<div className='apple-player'>
-			<div>
-				<button onClick={() => playSong()}>Play</button>
-				<button onClick={() => prevSong()}>Prev</button>
-				<button onClick={() => pauseSong()}>Pause</button>
-				<button onClick={() => nextSong()}>Next</button>
-			</div>
-			<AppleDisplay />
-			<div className='temp-ui'>
-				<AppleSearch
-					lobbyId={lobby_id}
-					setSong={setSong}
-					setAlbum={setAlbum}
-				/>
-				<AppleQueue queue={queue} />
-			</div>
-		</div>
-	);
+      await musicKit.authorize().then(async () => {
+        await musicKit.setQueue({
+          songs: arrayQueue,
+        });
+      });
+    }
+
+    //Displaying everysong
+    let displayQueue = [];
+    newQueue.map(({ attributes }) => {
+      displayQueue.push({
+        attributes: {
+          artistName: attributes.artistName,
+          name: attributes.name,
+        },
+      });
+    });
+    setQueue([...queue, ...displayQueue]);
+  }
+
+  // TEMP PLAYER CONTROLS --- FOR TESTING
+  let nextSong = async () => {
+    await musicKit.authorize().then(async () => {
+      await musicKit.skipToNextItem();
+    });
+  };
+  let prevSong = async () => {
+    await musicKit.authorize().then(async () => {
+      await musicKit.skipToPreviousItem();
+    });
+  };
+  let pauseSong = async () => {
+    await musicKit.authorize().then(async () => {
+      await musicKit.pause();
+    });
+  };
+
+  // TEMP PLAYER CONTROLS --- FOR TESTING
+
+  return (
+    <div className="apple-player">
+      <div>
+        <button onClick={() => playSong()}>Play</button>
+        <button onClick={() => prevSong()}>Prev</button>
+        <button onClick={() => pauseSong()}>Pause</button>
+        <button onClick={() => nextSong()}>Next</button>
+      </div>
+      <AppleDisplay />
+      <div class="temp-ui">
+        <AppleSearch lobbyId={lobby_id} setSong={setSong} setAlbum={setAlbum} />
+        <AppleQueue
+          queue={queue}
+          playSongByIndex={playSongByIndex}
+          removeItemFromQueue={removeItemFromQueue}
+          removeAllItemsFromQueue={removeAllItemsFromQueue}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ApplePlayer;
--- front-end/src/components/apple-queue/AppleQueue.jsx
+++ front-end/src/components/apple-queue/AppleQueue.jsx
@@ -1,13 +1,42 @@
-function AppleQueue({ queue }) {
+import "./AppleQueue.scss";
+function AppleQueue({
+  queue,
+  playSongByIndex,
+  removeItemFromQueue,
+  removeAllItemsFromQueue,
+}) {
   return (
     <div>
-      <h1>QUEUE</h1>
+      <div className="queue-menu">
+        <h1>QUEUE</h1>
+        <button
+          className="delete-btn"
+          onClick={() => removeAllItemsFromQueue()}
+        >
+          Delete all
+        </button>
+      </div>
       {queue.length === 0 ? (
         <div>No Queue</div>
       ) : (
-        queue.map((song) => (
-          <div>
-            {song.attributes.artistName} - {song.attributes.name}
+        queue.map((song, index) => (
+          <div className="queue-item">
+            <div
+              className="queue-name"
+              onClick={() => {
+                playSongByIndex(index);
+              }}
+            >
+              {song.attributes.artistName} - {song.attributes.name}
+            </div>
+            <button
+              className="delete-btn"
+              onClick={() => {
+                removeItemFromQueue(index);
+              }}
+            >
+              delete
+            </button>
           </div>
         ))
       )}
