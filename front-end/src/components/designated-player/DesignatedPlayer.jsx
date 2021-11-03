67590cce496024ed0873f141077c34d002b9a42a
Alexis Martin <alexismartin1895@gmail.com>
2021-11-02 19:00:30 -0700

    Fixed apple player instance on refresh

--- front-end/src/components/designated-player/DesignatedPlayer.jsx
+++ front-end/src/components/designated-player/DesignatedPlayer.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SpotifyPlayer from '../spotify-player/SpotifyPlayer';
 import ApplePlayer from '../../components/apple-player/ApplePlayer';
+import AppleLogin from '../apple-login/AppleLogin';
+import { AppleMusicContext } from '../../context/AppleMusicContext';
 
 function DesignatedPlayer({ musicProvider, token, lobby_id }) {
-	return musicProvider === 'spotify' ? (
-		<SpotifyPlayer token={token} lobby_id={lobby_id} />
-	) : (
-		<ApplePlayer lobby_id={lobby_id} />
-	);
+	const [musicKit] = useContext(AppleMusicContext);
+
+	if (musicProvider === 'spotify') {
+		return <SpotifyPlayer token={token} lobby_id={lobby_id} />;
+	} else {
+		return musicKit ? (
+			<ApplePlayer lobby_id={lobby_id} />
+		) : (
+			<AppleLogin />
+		);
+	}
 }
 
 export default DesignatedPlayer;
