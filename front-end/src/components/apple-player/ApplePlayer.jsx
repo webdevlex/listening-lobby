5dbcd00feec5065de4024d12f6ff4e1c2ab354f8
justinfisherrr <74376317+justinfisherrr@users.noreply.github.com>
2021-11-05 00:39:41 -0700

    queue ready for implementation

--- front-end/src/components/apple-player/ApplePlayer.jsx
+++ front-end/src/components/apple-player/ApplePlayer.jsx
@@ -2,14 +2,10 @@ import React, { useEffect, useContext, useState } from "react";
 import { SocketContext } from "../../context/socketContext";
 import { AppleMusicContext } from "../../context/AppleMusicContext";
 import "./apple-player.scss";
-import AppleDisplay from "../apple-display/AppleDisplay";
-import AppleQueue from "../apple-queue/AppleQueue";
-import AppleSearch from "../apple-search/AppleSearch";
+
 function ApplePlayer({ lobby_id }) {
   const socket = useContext(SocketContext);
   const [musicKit] = useContext(AppleMusicContext);
-  const [queue, setQueue] = useState([]);
-
   useEffect(() => {
     console.log(musicKit);
     // Apple Socket Functions
@@ -29,102 +25,20 @@ function ApplePlayer({ lobby_id }) {
     socket.emit("playSong", lobby_id);
   }
 
-  async function removeItemFromQueue(index) {
-    //Changing the display of the queue
-    let tempQueue = queue;
-    tempQueue.splice(index, 1);
-    await setQueue([...tempQueue]);
-
-    //Changing MusicKit Queue
-    let currentQueue = musicKit.player.queue.items;
-
-    //Resets Index of Music player
-    if (index < musicKit.player.queue.position) {
-      musicKit.player.queue.position = musicKit.player.queue.position - 1;
-    }
-
-    currentQueue.splice(index, 1);
-
-    await musicKit.authorize();
-    await musicKit.setQueue({
-      songs: currentQueue,
-    });
-  }
-
-  async function removeAllItemsFromQueue() {
-    //Changing the display of the queue
-
-    await setQueue([]);
-    //Changing MusicKit Queue
-    await musicKit.authorize();
-    await musicKit.setQueue({
-      songs: [],
-    });
-    await musicKit.pause();
-  }
-
-  async function playSongByIndex(index) {
-    await musicKit.authorize();
-    await musicKit.changeToMediaAtIndex(index);
-    musicKit.player.volume = 0.05;
-
-    playSong();
-  }
-
-  async function setSong(song) {
+  //All I need passed in an object that has an {href, id, type}
+  async function setMusicKitQueue(item) {
     await musicKit.authorize();
     let musicQueue = musicKit.player.queue;
-
-    musicQueue.append(song);
-
-    setQueue([
-      ...queue,
-      {
-        attributes: {
-          artistName: song.attributes.artistName,
-          name: song.attributes.name,
-        },
-      },
-    ]);
-  }
-
-  async function setAlbum(album) {
-    let currentQueue = musicKit.player.queue.items;
-    let arrayQueue = [];
-    currentQueue.forEach(({ id }) => {
-      arrayQueue.push(id);
-    });
-
-    //Added by album id (issue: clears entire queue before adding)
-    await musicKit.authorize();
-    await musicKit.setQueue({
-      album: album.id,
-    });
-
-    //If we have existing songs, we will push those
-    let newQueue = musicKit.player.queue.items;
-    if (arrayQueue.length > 0) {
-      newQueue.forEach(({ id }) => {
-        arrayQueue.push(id);
-      });
-
-      await musicKit.authorize();
-      await musicKit.setQueue({
-        songs: arrayQueue,
+    if (item.type === "songs") {
+      musicQueue.append(item);
+    } else if (item.type === "albums") {
+      let album = await musicKit.api.album(item.id);
+      let tracks = album.relationships.tracks.data;
+      let musicKitQueue = musicKit.player.queue;
+      tracks.forEach((song) => {
+        musicKitQueue.append(song);
       });
     }
-
-    //Displaying everysong
-    let displayQueue = [];
-    newQueue.forEach(({ attributes }) => {
-      displayQueue.push({
-        attributes: {
-          artistName: attributes.artistName,
-          name: attributes.name,
-        },
-      });
-    });
-    setQueue([...queue, ...displayQueue]);
   }
 
   // TEMP PLAYER CONTROLS --- FOR TESTING
@@ -151,18 +65,50 @@ function ApplePlayer({ lobby_id }) {
         <button onClick={() => pauseSong()}>Pause</button>
         <button onClick={() => nextSong()}>Next</button>
       </div>
-      <AppleDisplay />
-      <div class='temp-ui'>
-        <AppleSearch lobbyId={lobby_id} setSong={setSong} setAlbum={setAlbum} />
-        <AppleQueue
-          queue={queue}
-          playSongByIndex={playSongByIndex}
-          removeItemFromQueue={removeItemFromQueue}
-          removeAllItemsFromQueue={removeAllItemsFromQueue}
-        />
-      </div>
     </div>
   );
 }
 
 export default ApplePlayer;
+
+// Refrence functions:
+//  async function removeItemFromQueue(index) {
+//   //Changing the display of the queue
+//   let tempQueue = queue;
+//   tempQueue.splice(index, 1);
+//   await setQueue([...tempQueue]);
+
+//   //Changing MusicKit Queue
+//   let currentQueue = musicKit.player.queue.items;
+
+//   //Resets Index of Music player
+//   if (index < musicKit.player.queue.position) {
+//     musicKit.player.queue.position = musicKit.player.queue.position - 1;
+//   }
+
+//   currentQueue.splice(index, 1);
+
+//   await musicKit.authorize();
+//   await musicKit.setQueue({
+//     songs: currentQueue,
+//   });
+// }
+// async function playSongByIndex(index) {
+//   await musicKit.authorize();
+//   await musicKit.changeToMediaAtIndex(index);
+//   musicKit.player.volume = 0.05;
+
+//   playSong();
+// }
+
+// async function removeAllItemsFromQueue() {
+//   //Changing the display of the queue
+
+//   await setQueue([]);
+//   //Changing MusicKit Queue
+//   await musicKit.authorize();
+//   await musicKit.setQueue({
+//     songs: [],
+//   });
+//   await musicKit.pause();
+// }
--- sockets/helperFunctions.js
+++ sockets/helperFunctions.js
@@ -1,66 +1,67 @@
 function formatSearchResults(searchResults, music_provider) {
-	let necessaryTrackInfo;
-	let necessaryAlbumInfo;
+  let necessaryTrackInfo = [];
+  let necessaryAlbumInfo = [];
 
-	if (music_provider === 'spotify') {
-		const albums = searchResults.albums.items;
-		const tracks = searchResults.tracks.items;
-		console.log(albums[0]);
+  if (music_provider === "spotify") {
+    const albums = searchResults.albums.items;
+    const tracks = searchResults.tracks.items;
 
-		necessaryTrackInfo = tracks.map((track) => {
-			return {
-				trackName: track.name,
-				artists: track.artists.map(({ name }) => name).join(', '),
-				trackCover: track.album.images[0].url,
-				id: track.id,
-				uniId: track.external_ids.isrc,
-			};
-		});
+    necessaryTrackInfo = tracks.map((track) => {
+      return {
+        trackName: track.name,
+        artists: track.artists.map(({ name }) => name).join(", "),
+        trackCover: track.album.images[0].url,
+        id: track.id,
+        uniId: track.external_ids.isrc,
+      };
+    });
 
-		necessaryAlbumInfo = albums.map((album) => {
-			return {
-				albumName: album.name,
-				artists: album.artists.map(({ name }) => name).join(', '),
-				albumCover: album.images[0].url,
-				id: album.id,
-				songCount: album.total_tracks,
-			};
-		});
-	} else {
-		const albums = searchResults.albums.data;
-		const tracks = searchResults.songs.data;
+    necessaryAlbumInfo = albums.map((album) => {
+      return {
+        albumName: album.name,
+        artists: album.artists.map(({ name }) => name).join(", "),
+        albumCover: album.images[0].url,
+        id: album.id,
+        songCount: album.total_tracks,
+      };
+    });
+  } else {
+    if (searchResults.songs) {
+      const tracks = searchResults.songs.data;
+      necessaryTrackInfo = tracks.map((track) => {
+        return {
+          trackName: track.attributes.name,
+          artists: track.attributes.artistName,
+          trackCover: track.attributes.artwork.url.replace(
+            "{w}x{h}",
+            "640x640"
+          ),
+          id: track.id,
+          uniId: track.attributes.isrc,
+        };
+      });
+    }
+    if (searchResults.albums) {
+      const albums = searchResults.albums.data;
+      necessaryAlbumInfo = albums.map((album) => {
+        return {
+          albumName: album.attributes.name,
+          artists: album.attributes.artistName,
+          albumCover: album.attributes.artwork.url.replace(
+            "{w}x{h}",
+            "640x640"
+          ),
+          id: album.id,
+          songCount: album.attributes.trackCount,
+        };
+      });
+    }
+  }
 
-		necessaryTrackInfo = tracks.map((track) => {
-			return {
-				trackName: track.attributes.name,
-				artists: track.attributes.artistName,
-				trackCover: track.attributes.artwork.url.replace(
-					'{w}x{h}',
-					'640x640'
-				),
-				id: track.id,
-				uniId: track.attributes.isrc,
-			};
-		});
-
-		necessaryAlbumInfo = albums.map((album) => {
-			return {
-				albumName: album.attributes.name,
-				artists: album.attributes.artistName,
-				albumCover: album.attributes.artwork.url.replace(
-					'{w}x{h}',
-					'640x640'
-				),
-				id: album.id,
-				songCount: album.attributes.trackCount,
-			};
-		});
-	}
-
-	return { tracks: necessaryTrackInfo, albums: necessaryAlbumInfo };
+  return { tracks: necessaryTrackInfo, albums: necessaryAlbumInfo };
 }
 
 module.exports = {
-	formatSearchResults,
+  formatSearchResults,
 };
 exports = module.exports;
