6390f022e75ac9563b2dc97594ee776f46bca1db
justinfisherrr <74376317+justinfisherrr@users.noreply.github.com>
2021-11-25 09:54:16 -0800

    Added functional event listener for song change; Got musicKit queue to work with global queue; calibrated new togglePlay function to apple player

--- front-end/src/components/socket-handler/SocketHandler.jsx
+++ front-end/src/components/socket-handler/SocketHandler.jsx
@@ -1,59 +1,59 @@
-import React, { useContext, useEffect } from 'react';
-import { SocketContext } from '../../context/SocketContext';
+import React, { useContext, useEffect } from "react";
+import { SocketContext } from "../../context/SocketContext";
 
 function SocketHandler({
-	setUser,
-	setMembers,
-	setMessages,
-	setQueue,
-	setLoading,
+  setUser,
+  setMembers,
+  setMessages,
+  setQueue,
+  setLoading,
 }) {
-	const params = new URLSearchParams(window.location.search);
-	const token = params.get('token');
-	const refresh_token = params.get('refresh_token');
-	const [socket] = useContext(SocketContext);
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("token");
+  const refresh_token = params.get("refresh_token");
+  const [socket] = useContext(SocketContext);
 
-	useEffect(() => {
-		const localStorageData = JSON.parse(localStorage.getItem('user'));
-		setUser({ ...localStorageData, token, refresh_token });
+  useEffect(() => {
+    const localStorageData = JSON.parse(localStorage.getItem("user"));
+    setUser({ ...localStorageData, token, refresh_token });
 
-		socket.emit('joinLobby', {
-			lobby_id: localStorageData.lobby_id,
-			username: localStorageData.username,
-			token: token,
-			refresh_token: refresh_token,
-			music_provider: localStorageData.music_provider,
-		});
+    socket.emit("joinLobby", {
+      lobby_id: localStorageData.lobby_id,
+      username: localStorageData.username,
+      token: token,
+      refresh_token: refresh_token,
+      music_provider: localStorageData.music_provider,
+    });
 
-		socket.on('lobbyMessage', (lobbyMessages) => {
-			setMessages(lobbyMessages);
-		});
+    socket.on("lobbyMessage", (lobbyMessages) => {
+      setMessages(lobbyMessages);
+    });
 
-		socket.on('setLobbyInfo', (members, lobbyMessages) => {
-			setMembers(members);
-			setMessages(lobbyMessages);
-		});
+    socket.on("setLobbyInfo", (members, lobbyMessages) => {
+      setMembers(members);
+      setMessages(lobbyMessages);
+    });
 
-		socket.on('updateLobbyQueue', (queue) => {
-			console.log(queue);
-			setQueue(queue);
-		});
+    socket.on("updateLobbyQueue", (queue) => {
+      console.log(queue);
+      setQueue(queue);
+    });
 
-		socket.on('doneLoading', (playerData) => {
-			setLoading(false);
-			console.log(playerData);
-		});
-	}, [
-		socket,
-		token,
-		refresh_token,
-		setMembers,
-		setMessages,
-		setUser,
-		setQueue,
-	]);
+    socket.on("doneLoading", (playerData) => {
+      setLoading(false);
+      console.log(playerData);
+    });
+  }, [
+    socket,
+    token,
+    refresh_token,
+    setMembers,
+    setMessages,
+    setUser,
+    setQueue,
+  ]);
 
-	return null;
+  return null;
 }
 
 export default SocketHandler;
--- sockets/reciever.js
+++ sockets/reciever.js
@@ -1,61 +1,61 @@
-const handlers = require('./handlers');
+const handlers = require("./handlers");
 
 // Recives interactions from the front end and calls disignated handler
 function socketsReciever(io) {
-	io.sockets.on('connection', function (socket) {
-		console.log('----- connection -----');
+  io.sockets.on("connection", function (socket) {
+    console.log("----- connection -----");
 
-		// Handle when user creates or joins lobby
-		socket.on('joinLobby', async (data) => {
-			await handlers.handleJoinLobby(io, socket, data);
-		});
+    // Handle when user creates or joins lobby
+    socket.on("joinLobby", async (data) => {
+      await handlers.handleJoinLobby(io, socket, data);
+    });
 
-		// Handle when a spotify users player is finished loading, ( Only for spotify users )
-		socket.on('setDeviceId', (data) => {
-			handlers.handleSetDeviceId(io, socket, data);
-		});
+    // Handle when a spotify users player is finished loading, ( Only for spotify users )
+    socket.on("setDeviceId", (data) => {
+      handlers.handleSetDeviceId(io, socket, data);
+    });
 
-		// TODO: make sure used players gets updated on leave
-		socket.on('disconnect', async () => {
-			await handlers.handleDisconnect(io, socket);
-		});
+    // TODO: make sure used players gets updated on leave
+    socket.on("disconnect", async () => {
+      await handlers.handleDisconnect(io, socket);
+    });
 
-		// Handle when a user sends a message in their lobby
-		socket.on('lobbyMessage', (data) => {
-			handlers.handleLobbyMessage(io, socket, data);
-		});
+    // Handle when a user sends a message in their lobby
+    socket.on("lobbyMessage", (data) => {
+      handlers.handleLobbyMessage(io, socket, data);
+    });
 
-		// Handle when a user sends a Spotify or apple search request
-		socket.on('uniSearch', async (data) => {
-			await handlers.handleUniSearch(io, socket, data);
-		});
+    // Handle when a user sends a Spotify or apple search request
+    socket.on("uniSearch", async (data) => {
+      await handlers.handleUniSearch(io, socket, data);
+    });
 
-		// Handle when a user attempts to add a song to the queue
-		socket.on('addSongToQueue', async (data) => {
-			await handlers.handleAddSongToQueue(io, socket, data);
-		});
+    // Handle when a user attempts to add a song to the queue
+    socket.on("addSongToQueue", async (data) => {
+      await handlers.handleAddSongToQueue(io, socket, data);
+    });
 
-		// Handle wehn a user attempts to add an album to queue
-		socket.on('addAlbumToQueue', async (data) => {
-			await handlers.handleAddAlbumToQueue(io, socket, data);
-		});
+    // Handle wehn a user attempts to add an album to queue
+    socket.on("addAlbumToQueue", async (data) => {
+      await handlers.handleAddAlbumToQueue(io, socket, data);
+    });
 
-		// Handle when a user hits play
-		socket.on('togglePlay', (data) => {
-			handlers.handleTogglePlay(io, socket, data);
-		});
+    // Handle when a user hits play
+    socket.on("togglePlay", (data) => {
+      handlers.handleTogglePlay(io, socket, data);
+    });
 
-		// Handle when a user hits play next song
-		socket.on('skip', (data) => {
-			handlers.handleSkip(io, socket, data);
-		});
+    // Handle when a user hits play next song
+    socket.on("skip", (data) => {
+      handlers.handleSkip(io, socket, data);
+    });
 
-		socket.on('playerData', (data) => {
-			handlers.handlePlayerData(io, socket, data);
-		});
+    socket.on("playerData", (data) => {
+      handlers.handlePlayerData(io, socket, data);
+    });
 
-		// TODO Handle when a user likes a song
-	});
+    // TODO Handle when a user likes a song
+  });
 }
 
 module.exports = socketsReciever;
