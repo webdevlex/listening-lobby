60d6b72b54305009f311ff76d2fb123bd6e33549
justinfisherrr <74376317+justinfisherrr@users.noreply.github.com>
2021-12-23 20:05:31 -0800

    Changed musickit to v3

--- front-end/src/components/apple-player/ApplePlayer.jsx
+++ front-end/src/components/apple-player/ApplePlayer.jsx
@@ -1,191 +1,193 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { SocketContext } from '../../context/SocketContext';
-import { PlayersContext } from '../../context/PlayersContext';
-import './apple-player.scss';
-import { setupSocketRecievers } from '../apple-player/recievers';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useContext, useState } from "react";
+import { SocketContext } from "../../context/SocketContext";
+import { PlayersContext } from "../../context/PlayersContext";
+import "./apple-player.scss";
+import { setupSocketRecievers } from "../apple-player/recievers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faPlay,
-	faPause,
-	faStepBackward,
-	faStepForward,
-	faVolumeUp,
-	faHeart,
-} from '@fortawesome/free-solid-svg-icons';
-import LoadingSpinner from '../loading-spinner/LoadingSpinner';
-import { faHeart as heartOutline } from '@fortawesome/free-regular-svg-icons';
+  faPlay,
+  faPause,
+  faStepBackward,
+  faStepForward,
+  faVolumeUp,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
+import LoadingSpinner from "../loading-spinner/LoadingSpinner";
+import { faHeart as heartOutline } from "@fortawesome/free-regular-svg-icons";
 
 function ApplePlayer({
-	user,
-	playerStatus,
-	queue,
-	buttonsClickable,
-	loading,
-	likedSongs,
-	setLikedSongs,
-	playing,
-	setPlaying,
+  user,
+  playerStatus,
+  queue,
+  buttonsClickable,
+  loading,
+  likedSongs,
+  setLikedSongs,
+  playing,
+  setPlaying,
 }) {
-	const [volume, setVolume] = useState(10);
-	const [socket] = useContext(SocketContext);
-	const { apple } = useContext(PlayersContext);
-	const [applePlayer] = apple;
-	const [percent, setPercent] = useState(0);
-	const [currentTime, setCurrentTime] = useState(0);
-	const [ran, setRan] = useState(false);
-	const song = queue[0] ? queue[0] : null;
+  const [volume, setVolume] = useState(10);
+  const [socket] = useContext(SocketContext);
+  const { apple } = useContext(PlayersContext);
+  const [applePlayer] = apple;
+  const [percent, setPercent] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [ran, setRan] = useState(false);
+  const song = queue[0] ? queue[0] : null;
 
-	useEffect(() => {
-		if (!ran) {
-			setupSocketRecievers(
-				applePlayer,
-				socket,
-				user,
-				setPlaying,
-				playerStatus,
-				queue,
-				setPercent,
-				setCurrentTime
-			);
-			setRan(true);
-		}
-	}, [applePlayer, user, playerStatus, queue, ran, setRan, socket, setPlaying]);
+  useEffect(() => {
+    if (!ran) {
+      setupSocketRecievers(
+        applePlayer,
+        socket,
+        user,
+        setPlaying,
+        playerStatus,
+        queue,
+        setPercent,
+        setCurrentTime
+      );
+      setRan(true);
+    }
+  }, [applePlayer, user, playerStatus, queue, ran, setRan, socket, setPlaying]);
 
-	let play = () => {
-		socket.emit('play', { user });
-	};
+  let play = () => {
+    socket.emit("play", { user });
+  };
 
-	let skip = async () => {
-		socket.emit('skip', { user });
-	};
+  let skip = async () => {
+    socket.emit("skip", { user });
+  };
 
-	// let getInstance = async () => {
-	// 	console.log(applePlayer);
-	// };
+  let getInstance = async () => {
+    console.log(applePlayer);
+  };
 
-	let updateVolume = (e, data) => {
-		applePlayer.player.volume = e.target.value / 100;
-		setVolume(data);
+  let updateVolume = (e, data) => {
+    applePlayer.volume = e.target.value / 100;
+    setVolume(data);
 
-		// Animation
-		let target = e.target;
-		if (e.target.type !== 'range') {
-			target = document.getElementById('range');
-		}
-		const min = target.min;
-		const max = target.max;
-		const val = target.value;
+    // Animation
+    let target = e.target;
+    if (e.target.type !== "range") {
+      target = document.getElementById("range");
+    }
+    const min = target.min;
+    const max = target.max;
+    const val = target.value;
 
-		target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
-	};
+    target.style.backgroundSize = ((val - min) * 100) / (max - min) + "% 100%";
+  };
 
-	async function addSongToLibrary(spotifySong, appleSong, id) {
-		setLikedSongs([...likedSongs, id]);
-		if (user.music_provider === 'apple') {
-			await applePlayer.addToLibrary(appleSong);
-		} else {
-			socket.emit('likeSong', { spotifySong, user });
-		}
-	}
+  async function addSongToLibrary(spotifySong, appleSong, id) {
+    setLikedSongs([...likedSongs, id]);
+    if (user.music_provider === "apple") {
+      await applePlayer.addToLibrary(appleSong);
+    } else {
+      socket.emit("likeSong", { spotifySong, user });
+    }
+  }
 
-	function formatDuration(millis) {
-		var minutes = Math.floor(millis / 60000);
-		var seconds = ((millis % 60000) / 1000).toFixed(0);
-		return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-	}
+  function formatDuration(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  }
 
-	return loading ? null : (
-		<div className='player-bar'>
-			<div className='player-left'>
-				{song ? (
-					<>
-						<div className='album-cover-container'>
-							<img className='album-cover' src={song.ui.trackCover} alt='' />
-						</div>
-						<div className='text'>
-							<p className='simple-text track-title'>{song.ui.trackName}</p>
-							<p className='simple-text track-artists'>{song.ui.artists}</p>
-						</div>
-						<div className='like-icon-container'>
-							{likedSongs.includes(queue[0].ui.id) ? (
-								<FontAwesomeIcon className='like-icon' icon={faHeart} />
-							) : (
-								<FontAwesomeIcon
-									className='like-icon'
-									icon={heartOutline}
-									onClick={() => {
-										addSongToLibrary(
-											queue[0].spotify,
-											queue[0].apple,
-											queue[0].ui.id
-										);
-									}}
-								/>
-							)}
-						</div>
-					</>
-				) : (
-					<>
-						<div className='album-cover-container'>
-							<p className='default-album-cover'>?</p>
-						</div>
-						<div className='text'>
-							<p className='simple-text track-title'>No Songs Added</p>
-							<p className='simple-text track-artists'>
-								Search and add songs to queue!
-							</p>
-						</div>
-					</>
-				)}
-			</div>
-			<div className='player-center'>
-				<div className='player-controls-container'>
-					{buttonsClickable ? (
-						<div className='player-controls'>
-							<FontAwesomeIcon className='skip-icon' icon={faStepBackward} />
-							<button className='play-button' onClick={() => play()}>
-								{playing ? (
-									<FontAwesomeIcon className='action-icon' icon={faPause} />
-								) : (
-									<FontAwesomeIcon className='action-icon' icon={faPlay} />
-								)}
-							</button>
-							<FontAwesomeIcon
-								className='skip-icon'
-								onClick={() => skip()}
-								icon={faStepForward}
-							/>
-						</div>
-					) : (
-						<LoadingSpinner />
-					)}
-				</div>
-				<div className='time-bar-container'>
-					<p className='current-time time'>{formatDuration(currentTime)}</p>
-					<div className='time-bar'>
-						<div
-							className='time-bar-slider'
-							style={{ left: `${percent}%` }}></div>
-					</div>
-					<p className='total-time time'>
-						{song ? song.ui.formattedDuration : '0:00'}
-					</p>
-				</div>
-			</div>
-			<div className='player-right'>
-				{/* <button onClick={() => getInstance()}>Get Instance</button> */}
-				<FontAwesomeIcon className='action-icon' icon={faVolumeUp} />
-				<input
-					className='volume-slider'
-					type='range'
-					min='0'
-					max='100'
-					defaultValue={volume}
-					onChange={updateVolume}
-				/>
-			</div>
-		</div>
-	);
+  return loading ? null : (
+    <div className='player-bar'>
+      <div className='player-left'>
+        {song ? (
+          <>
+            <div className='album-cover-container'>
+              <img className='album-cover' src={song.ui.trackCover} alt='' />
+            </div>
+            <div className='text'>
+              <p className='simple-text track-title'>{song.ui.trackName}</p>
+              <p className='simple-text track-artists'>{song.ui.artists}</p>
+            </div>
+            <div className='like-icon-container'>
+              {likedSongs.includes(queue[0].ui.id) ? (
+                <FontAwesomeIcon className='like-icon' icon={faHeart} />
+              ) : (
+                <FontAwesomeIcon
+                  className='like-icon'
+                  icon={heartOutline}
+                  onClick={() => {
+                    addSongToLibrary(
+                      queue[0].spotify,
+                      queue[0].apple,
+                      queue[0].ui.id
+                    );
+                  }}
+                />
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className='album-cover-container'>
+              <p className='default-album-cover'>?</p>
+            </div>
+            <div className='text'>
+              <p className='simple-text track-title'>No Songs Added</p>
+              <p className='simple-text track-artists'>
+                Search and add songs to queue!
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+      <div className='player-center'>
+        <div className='player-controls-container'>
+          {buttonsClickable ? (
+            <div className='player-controls'>
+              <FontAwesomeIcon className='skip-icon' icon={faStepBackward} />
+              <button className='play-button' onClick={() => play()}>
+                {playing ? (
+                  <FontAwesomeIcon className='action-icon' icon={faPause} />
+                ) : (
+                  <FontAwesomeIcon className='action-icon' icon={faPlay} />
+                )}
+              </button>
+              <FontAwesomeIcon
+                className='skip-icon'
+                onClick={() => skip()}
+                icon={faStepForward}
+              />
+            </div>
+          ) : (
+            <LoadingSpinner />
+          )}
+        </div>
+        <div className='time-bar-container'>
+          <p className='current-time time'>{formatDuration(currentTime)}</p>
+          <div className='time-bar'>
+            <div
+              className='time-bar-slider'
+              style={{ left: `${percent}%` }}
+            ></div>
+          </div>
+          <p className='total-time time'>
+            {song ? song.ui.formattedDuration : "0:00"}
+          </p>
+        </div>
+      </div>
+      <button onClick={() => getInstance()}></button>
+      <div className='player-right'>
+        {/* <button onClick={() => getInstance()}>Get Instance</button> */}
+        <FontAwesomeIcon className='action-icon' icon={faVolumeUp} />
+        <input
+          className='volume-slider'
+          type='range'
+          min='0'
+          max='100'
+          defaultValue={volume}
+          onChange={updateVolume}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ApplePlayer;
--- front-end/src/components/apple-player/handlers.js
+++ front-end/src/components/apple-player/handlers.js
@@ -1,24 +1,25 @@
 //Adds and removes event listeners
 let removeEventListener = (applePlayer) => {
-	applePlayer.removeEventListener('mediaItemDidChange', () => {});
+  // applePlayer.services.removeEventListener("mediaItemDidChange", () => {});
 };
 let addEventListener = (applePlayer, socket, user, toAdd) => {
-	applePlayer.addEventListener('mediaItemDidChange', () => {
-		socket.emit('mediaChange', { user });
-	});
-	if (toAdd) {
-		applePlayer.player.addEventListener('playbackStateDidChange', () => {
-			if (applePlayer.player.playbackState === 10) {
-				socket.emit('mediaChange', { user });
-			}
-		});
-		applePlayer.player.addEventListener('playbackBitrateDidChange', () => {
-			if (applePlayer.bitrate === -1) {
-				localStorage.setItem('playback', JSON.stringify({ changed: true }));
-				window.location.replace('http://localhost:3000');
-			}
-		});
-	}
+  applePlayer.addEventListener("playbackSkip", () => {
+    socket.emit("mediaChange", { user });
+  });
+
+  if (toAdd) {
+    applePlayer.addEventListener("playbackStateDidChange", () => {
+      if (applePlayer.playbackState === 10) {
+        socket.emit("mediaChange", { user });
+      }
+    });
+    // applePlayer.player.addEventListener("playbackBitrateDidChange", () => {
+    //   if (applePlayer.bitrate === -1) {
+    //     localStorage.setItem("playback", JSON.stringify({ changed: true }));
+    //     window.location.replace("http://localhost:3000");
+    //   }
+    // });
+  }
 };
 
 //Variables for joining lobby
@@ -28,215 +29,216 @@ let joinOnPause = false;
 //Helpers
 //Sets musicQueue
 async function setMusicKitQueue(applePlayer, id) {
-	await applePlayer.authorize();
-	await applePlayer.setQueue({
-		song: id,
-	});
+  await applePlayer.authorize();
+  await applePlayer.setQueue({
+    song: id,
+  });
 }
 
 //Start up function that sets intial values, listeners and catches up to existing
 //players
 export async function startUp(
-	applePlayer,
-	socket,
-	user,
-	playerStatus,
-	queue,
-	setPlaying,
-	setPercent,
-	setCurrentTime
+  applePlayer,
+  socket,
+  user,
+  playerStatus,
+  queue,
+  setPlaying,
+  setPercent,
+  setCurrentTime
 ) {
-	addEventListener(applePlayer, socket, user, true);
-	applePlayer.player.volume = 0.1;
-	if (playerStatus && queue.length > 0 && queue[0].apple !== '-1') {
-		await setMusicKitQueue(applePlayer, queue[0].apple);
-		timeStampOnJoin = playerStatus.timestamp / 1000;
-		if (!playerStatus.paused) {
-			await handlePlay(
-				applePlayer,
-				socket,
-				user,
-				setPlaying,
-				queue[0],
-				setPercent,
-				setCurrentTime
-			);
-			await applePlayer.seekToTime(timeStampOnJoin);
-			setPlaying(true);
-		} else {
-			joinOnPause = true;
-			socket.emit('userReady', { user });
-		}
-	} else if (!user.admin) {
-		socket.emit('userReady', { user });
-	}
+  console.log(applePlayer);
+  addEventListener(applePlayer, socket, user, true);
+  applePlayer.volume = 0.1;
+  if (playerStatus && queue.length > 0 && queue[0].apple !== "-1") {
+    await setMusicKitQueue(applePlayer, queue[0].apple);
+    timeStampOnJoin = playerStatus.timestamp / 1000;
+    if (!playerStatus.paused) {
+      await handlePlay(
+        applePlayer,
+        socket,
+        user,
+        setPlaying,
+        queue[0],
+        setPercent,
+        setCurrentTime
+      );
+      await applePlayer.seekToTime(timeStampOnJoin);
+      setPlaying(true);
+    } else {
+      joinOnPause = true;
+      socket.emit("userReady", { user });
+    }
+  } else if (!user.admin) {
+    socket.emit("userReady", { user });
+  }
 }
 //Sends other players playerData
 export function handleGetPlayerData(
-	applePlayer,
-	memberId,
-	{ lobby_id },
-	socket
+  applePlayer,
+  memberId,
+  { lobby_id },
+  socket
 ) {
-	socket.emit('playerData', {
-		paused: !applePlayer.player.isPlaying,
-		timestamp: applePlayer.player.currentPlaybackTime * 1000,
-		lobby_id,
-		memberId,
-	});
+  socket.emit("playerData", {
+    paused: !applePlayer.isPlaying,
+    timestamp: applePlayer.currentPlaybackTime * 1000,
+    lobby_id,
+    memberId,
+  });
 }
 
 //Handles play
 export async function handlePlay(
-	applePlayer,
-	socket,
-	user,
-	setPlaying,
-	song,
-	setPercent,
-	setCurrentTime
+  applePlayer,
+  socket,
+  user,
+  setPlaying,
+  song,
+  setPercent,
+  setCurrentTime
 ) {
-	await applePlayer.authorize();
-	removeEventListener(applePlayer);
-	if (song.apple !== '-1') {
-		await applePlayer.play();
+  await applePlayer.authorize();
+  removeEventListener(applePlayer);
+  if (song.apple !== "-1") {
+    await applePlayer.play();
 
-		if (joinOnPause) {
-			await applePlayer.seekToTime(timeStampOnJoin);
-			joinOnPause = false;
-		}
-		emitReadyWhenPlaying(socket, applePlayer, user);
-	} else {
-		emitUserReady(socket, user);
-	}
-	console.log('play');
-	moveTimeStamp(song, setPercent, setCurrentTime);
-	setPlaying(true);
-	addEventListener(applePlayer, socket, user, false);
+    if (joinOnPause) {
+      await applePlayer.seekToTime(timeStampOnJoin);
+      joinOnPause = false;
+    }
+    emitReadyWhenPlaying(socket, applePlayer, user);
+  } else {
+    emitUserReady(socket, user);
+  }
+  console.log("play");
+  moveTimeStamp(song, setPercent, setCurrentTime);
+  setPlaying(true);
+  addEventListener(applePlayer, socket, user, false);
 }
 
 //Handles pause
 export async function handlePause(applePlayer, socket, user, setPlaying, song) {
-	await applePlayer.authorize();
-	removeEventListener(applePlayer);
+  await applePlayer.authorize();
+  removeEventListener(applePlayer);
 
-	if (song.apple !== '-1') {
-		await applePlayer.pause();
-		emitReadyWhenPaused(socket, applePlayer, user);
-	} else {
-		emitUserReady(socket, user);
-	}
+  if (song.apple !== "-1") {
+    await applePlayer.pause();
+    emitReadyWhenPaused(socket, applePlayer, user);
+  } else {
+    emitUserReady(socket, user);
+  }
 
-	pauseTimeStamp();
-	setPlaying(false);
-	addEventListener(applePlayer, socket, user, false);
+  pauseTimeStamp();
+  setPlaying(false);
+  addEventListener(applePlayer, socket, user, false);
 }
 
 //Handles first song add
 export async function handleFirstSong(applePlayer, song, socket, user) {
-	if (song.apple !== '-1') {
-		await setMusicKitQueue(applePlayer, song.apple);
-		emitReadyWhenQueueSet(socket, applePlayer, user);
-	} else {
-		emitUserReady(socket, user);
-	}
+  if (song.apple !== "-1") {
+    await setMusicKitQueue(applePlayer, song.apple);
+    emitReadyWhenQueueSet(socket, applePlayer, user);
+  } else {
+    emitUserReady(socket, user);
+  }
 }
 
 //Handles popping
 export async function handlePopped(
-	applePlayer,
-	socket,
-	song,
-	user,
-	setPlaying,
-	setPercent,
-	setCurrentTime
+  applePlayer,
+  socket,
+  song,
+  user,
+  setPlaying,
+  setPercent,
+  setCurrentTime
 ) {
-	console.log('popped');
-	resetTimeStamp(setPercent, setCurrentTime);
-	pauseTimeStamp(setPercent, setCurrentTime);
-	if (song.apple !== '-1') {
-		await setMusicKitQueue(applePlayer, song.apple);
-		handlePlay(
-			applePlayer,
-			socket,
-			user,
-			setPlaying,
-			song,
-			setPercent,
-			setCurrentTime
-		);
-	} else {
-		handlePause(applePlayer, socket, user, setPlaying, song);
-	}
+  console.log("popped");
+  resetTimeStamp(setPercent, setCurrentTime);
+  pauseTimeStamp(setPercent, setCurrentTime);
+  if (song.apple !== "-1") {
+    await setMusicKitQueue(applePlayer, song.apple);
+    handlePlay(
+      applePlayer,
+      socket,
+      user,
+      setPlaying,
+      song,
+      setPercent,
+      setCurrentTime
+    );
+  } else {
+    handlePause(applePlayer, socket, user, setPlaying, song);
+  }
 }
 //Handles empty queue
 export async function handleEmptyQueue(
-	applePlayer,
-	socket,
-	user,
-	setPlaying,
-	setPercent,
-	setCurrentTime
+  applePlayer,
+  socket,
+  user,
+  setPlaying,
+  setPercent,
+  setCurrentTime
 ) {
-	await applePlayer.pause();
-	resetTimeStamp(setPercent, setCurrentTime);
-	pauseTimeStamp();
-	emitUserReady(socket, user);
-	setPlaying(false);
+  await applePlayer.pause();
+  resetTimeStamp(setPercent, setCurrentTime);
+  pauseTimeStamp();
+  emitUserReady(socket, user);
+  setPlaying(false);
 }
 
 export async function handleRemoveFirst(
-	applePlayer,
-	socket,
-	user,
-	setPlaying,
-	song,
-	isPlaying,
-	setPercent,
-	setCurrentTime
+  applePlayer,
+  socket,
+  user,
+  setPlaying,
+  song,
+  isPlaying,
+  setPercent,
+  setCurrentTime
 ) {
-	if (song.apple !== '-1') {
-		await setMusicKitQueue(applePlayer, song.apple);
-		if (isPlaying) {
-			handlePlay(applePlayer, socket, user, setPlaying, song);
-		} else {
-			handlePause(applePlayer, socket, user, setPlaying, song);
-		}
-	} else {
-		if (isPlaying) {
-			pauseTimeStamp();
-			await applePlayer.pause();
-			setPlaying(true);
-		} else {
-			setPlaying(false);
-		}
-		emitUserReady(socket, user);
-	}
-	resetTimeStamp(setPercent, setCurrentTime);
+  if (song.apple !== "-1") {
+    await setMusicKitQueue(applePlayer, song.apple);
+    if (isPlaying) {
+      handlePlay(applePlayer, socket, user, setPlaying, song);
+    } else {
+      handlePause(applePlayer, socket, user, setPlaying, song);
+    }
+  } else {
+    if (isPlaying) {
+      pauseTimeStamp();
+      await applePlayer.pause();
+      setPlaying(true);
+    } else {
+      setPlaying(false);
+    }
+    emitUserReady(socket, user);
+  }
+  resetTimeStamp(setPercent, setCurrentTime);
 }
 
 // Ready emitters
 function emitReadyWhenPlaying(socket, applePlayer, user) {
-	delay(() => socket.emit('userReady', { user }));
+  delay(() => socket.emit("userReady", { user }));
 }
 
 function emitReadyWhenPaused(socket, applePlayer, user) {
-	delay(() => socket.emit('userReady', { user }));
+  delay(() => socket.emit("userReady", { user }));
 }
 
 function emitReadyWhenQueueSet(socket, applePlayer, user) {
-	delay(() => socket.emit('userReady', { user }));
+  delay(() => socket.emit("userReady", { user }));
 }
 
 function emitUserReady(socket, user) {
-	delay(() => socket.emit('userReady', { user }));
+  delay(() => socket.emit("userReady", { user }));
 }
 
 function delay(func) {
-	setTimeout(() => {
-		func();
-	}, 500);
+  setTimeout(() => {
+    func();
+  }, 500);
 }
 
 // Timestamp
@@ -244,28 +246,28 @@ let currentTime = 0;
 let percent = 0;
 let interval = null;
 function moveTimeStamp(song, setPercent, setCurrentTime) {
-	let INTERVAL = song.ui.duration / 100;
-	if (!interval) {
-		interval = setInterval(() => {
-			if (percent < 100) {
-				percent += 1;
-				currentTime += INTERVAL;
-				setCurrentTime(currentTime);
-				setPercent(percent);
-				console.log(INTERVAL);
-			}
-		}, INTERVAL);
-	}
+  let INTERVAL = song.ui.duration / 100;
+  if (!interval) {
+    interval = setInterval(() => {
+      if (percent < 100) {
+        percent += 1;
+        currentTime += INTERVAL;
+        setCurrentTime(currentTime);
+        setPercent(percent);
+        console.log(INTERVAL);
+      }
+    }, INTERVAL);
+  }
 }
 
 function pauseTimeStamp() {
-	clearInterval(interval);
-	interval = null;
+  clearInterval(interval);
+  interval = null;
 }
 
 function resetTimeStamp(setPercent, setCurrentTime) {
-	currentTime = 0;
-	percent = 0;
-	setPercent(0);
-	setCurrentTime(0);
+  currentTime = 0;
+  percent = 0;
+  setPercent(0);
+  setCurrentTime(0);
 }
