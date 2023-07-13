e8d11230d13e76fb025e1a231b58e745b64c318c
Alexis Martin <alexismartin1895@gmail.com>
2023-07-13 12:15:39 -0700

    rolling back

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const http = require('http');
-const socketio = require('socket.io');
-const reciever = require('./sockets/reciever');
-const path = require('path');
-const frontEndUrl = '' || 'http://localhost:3000';
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const socketio = require("socket.io");
+const reciever = require("./sockets/reciever");
+const path = require("path");
+const frontEndUrl = "" || "http://localhost:3000";
 
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8888'];
+const whitelist = ["http://localhost:3000", "http://localhost:8888"];
 const corsOptions = {
 	origin: function (origin, callback) {
 		// console.log('** Origin of request ' + origin);
@@ -18,27 +18,27 @@ const corsOptions = {
 			callback(null, true);
 		} else {
 			// console.log('Origin rejected');
-			callback(new Error('Not allowed by CORS'));
+			callback(new Error("Not allowed by CORS"));
 		}
 	},
 };
 app
 	.use(cors(corsOptions))
 	.use(cookieParser())
-	.use('/spotify', require('./routes/spotify'))
-	.use('/apple', require('./routes/apple'));
+	.use("/spotify", require("./routes/spotify"))
+	.use("/apple", require("./routes/apple"));
 
 const server = http.createServer(app);
 const ioCors = {
-	cors: { origin: frontEndUrl, methods: ['GET', 'POST'] },
+	cors: { origin: frontEndUrl, methods: ["GET", "POST"] },
 };
 const io = socketio(server, ioCors);
 reciever(io);
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'front-end/build')));
-	app.get('*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'front-end/build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "front-end/build")));
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "front-end/build", "index.html"));
 	});
 }
 
